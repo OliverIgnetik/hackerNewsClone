701df80b6927402395251829feda7459230f6f71
Oliver Ignetik <olli@ozemail.com.au>
2019-08-04 16:15:34 +1000

    Added ant design package

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react'
-
+import 'antd/dist/antd.css'
+import {Layout} from 'antd'
 import glam, { Div } from 'glamorous'
-import { css } from 'glamor'
 import { Route, Switch, Link, Redirect } from 'react-router-dom'
 import logo from './logo.svg'
 import './App.css'
 
+// styled links with import from react router
+// you can pass in the link as the indicator for glam
+const StyledLink = glam(Link)({
+  color: 'white',
+  // destructure the props 
+  // color: color ~ color 
+},({color})=>({color}))
+
 const Posts = () => <p>Posts</p>
 
 // glamorous object for CSS
-// glam.any_HTML_element
 const AppHeader = glam.header({
   backgroundColor: '#282c34',
   minHeight: '100vh',
@@ -26,27 +33,17 @@ function App() {
     <Div css={{ textAlign: 'center' }}>
       <AppHeader>
         <img src={logo} className='App-logo' alt='logo' />
-
-        {/* Switch ensures both components do not render */}
-        <Switch>
-          {/* only match when the path is exactly this */}
-          {/* redirect the user to the posts page */}
-          <Redirect exact path='/' to='/posts' />
-          <Route path='/posts' component={Posts} />
-        </Switch>
-
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
         {/* anchor tags to endpoints */}
-        <Link to='/'>Home</Link>
-        <Link to='/posts'>Posts</Link>
+        <StyledLink to='/'>Home</StyledLink>
+        <StyledLink to='/posts'>Posts</StyledLink>
       </AppHeader>
+      {/* Switch ensures both components do not render */}
+      <Switch>
+        {/* only match when the path is exactly this */}
+        {/* redirect the user to the posts page */}
+        <Redirect exact path='/' to='/posts' />
+        <Route exact path='/posts' component={Posts} />
+      </Switch>
     </Div>
   )
 }
