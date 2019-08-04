bd66c437282685c09c4d4080462658ccc6a8fad8
Oliver Ignetik <olli@ozemail.com.au>
2019-08-04 17:09:46 +1000

    state reducer

--- client/src/index.js
+++ client/src/index.js
@@ -7,13 +7,42 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 
 // simple reducer
-const numbers = [1,2,3]
-const sum = numbers.reduce((acc,value)=>{
-    console.log({acc,value})
-    return acc + value
-},0)
+const actions = [
+  {
+    type: 'increment',
+    value: 1,
+  },
+  {
+    type: 'increment',
+    value: 3,
+  },
+  {
+    type: 'decrement',
+    value: 2,
+  },
+]
+const endState = actions.reduce(
+  (state, action) => {
+    if (action.type === 'increment') {
+      console.log('incrementing')
+      const { value } = state
+      console.log({ state, action })
+      // copy the state with a spread
+      return { ...state, value: value + action.value }
+    }
+    if (action.type === 'decrement') {
+      console.log('decrementing')
+      const { value } = state
+      console.log({ state, action })
+      // copy the state with a spread
+      return { ...state, value: value - action.value }
+    }
+    return state
+  },
+  { value: 0 },
+)
 
-console.log({sum})
+console.log({ endState })
 
 ReactDOM.render(
   <BrowserRouter>
