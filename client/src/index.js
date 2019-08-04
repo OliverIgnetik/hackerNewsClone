71bc847fb5b9d549e0928e8ffc26905ed761699e
Oliver Ignetik <olli@ozemail.com.au>
2019-08-04 17:22:12 +1000

    Installed redux and created a store

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+
+import { createStore } from 'redux'
 // gives powers to the app
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom'
@@ -6,43 +8,26 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-// simple reducer
-const actions = [
-  {
-    type: 'increment',
-    value: 1,
-  },
-  {
-    type: 'increment',
-    value: 3,
-  },
-  {
-    type: 'decrement',
-    value: 2,
-  },
-]
-const endState = actions.reduce(
-  (state, action) => {
-    if (action.type === 'increment') {
-      console.log('incrementing')
-      const { value } = state
-      console.log({ state, action })
-      // copy the state with a spread
-      return { ...state, value: value + action.value }
-    }
-    if (action.type === 'decrement') {
-      console.log('decrementing')
-      const { value } = state
-      console.log({ state, action })
-      // copy the state with a spread
-      return { ...state, value: value - action.value }
-    }
-    return state
-  },
-  { value: 0 },
-)
+// reducer
+const reducer = (state, action) => {
+  if (action.type === 'increment') {
+    const { value } = state
+    // copy the state with a spread
+    return { ...state, value: value + action.value }
+  }
+  if (action.type === 'decrement') {
+    const { value } = state
+    // copy the state with a spread
+    return { ...state, value: value - action.value }
+  }
+  return state
+}
 
-console.log({ endState })
+// createStore with initial state
+const store = createStore(reducer, {})
+
+console.log({store});
+
 
 ReactDOM.render(
   <BrowserRouter>
